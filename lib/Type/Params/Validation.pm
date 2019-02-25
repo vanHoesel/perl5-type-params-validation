2ef5f712683c196ffd1ae9796ed34798c54cf322
Theo van Hoesel <Th.J.v.Hoesel@gmail.com>
2019-02-26 00:01:20 +0100

    fix test "... dies when passing bad values"

--- lib/Type/Params/Validation.pm
+++ lib/Type/Params/Validation.pm
@@ -3,9 +3,19 @@ package Type::Params::Validation;
 use strict;
 use warnings;
 
+use Type::Params ();
+
 use Exporter 'import';
 our @EXPORT_OK = qw/&compile_named/;
 
-sub compile_named { return sub { return +{ @_ } } }
+sub compile_named {
+    my @checks = @_;
+    my $check = Type::Params::compile_named(@checks);
+    
+    return sub {
+        return $check->(@_)
+    }
+    
+}
 
 1;
