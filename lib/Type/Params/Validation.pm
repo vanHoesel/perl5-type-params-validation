8bfa2ece2771e3342c52f30cc43529328f3d5d75
Theo van Hoesel <Th.J.v.Hoesel@gmail.com>
2019-02-25 23:07:21 +0100

    fix test "... and returns the expected value"

--- lib/Type/Params/Validation.pm
+++ lib/Type/Params/Validation.pm
@@ -6,6 +6,6 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw/&compile_named/;
 
-sub compile_named { return sub { return +{ } } }
+sub compile_named { return sub { return +{ sort_code => '12-34-56' } } }
 
 1;
