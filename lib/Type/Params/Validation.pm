ae7ee1a4896d96b4a97ac957eb4c5a06ddf68b94
Theo van Hoesel <Th.J.v.Hoesel@gmail.com>
2019-02-26 11:34:00 +0100

    fix test "isa 'Error::TypeTiny::Validation'"

--- lib/Type/Params/Validation.pm
+++ lib/Type/Params/Validation.pm
@@ -8,12 +8,21 @@ use Type::Params ();
 use Exporter 'import';
 our @EXPORT_OK = qw/&compile_named/;
 
+use Try::Tiny;
+
 sub compile_named {
     my @checks = @_;
     my $check = Type::Params::compile_named(@checks);
     
     return sub {
-        return $check->(@_)
+        my @params = @_;
+        
+        try {
+            return $check->(@params);
+        } catch {
+            require Error::TypeTiny::Validation;
+            Error::TypeTiny::Validation->throw( );
+        };
     }
     
 }
