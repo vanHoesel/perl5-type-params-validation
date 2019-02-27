12d062cbe4625e279b57586a029ecf7797d026bf
Theo van Hoesel <Th.J.v.Hoesel@gmail.com>
2019-02-27 14:15:32 +0100

    add `_build_message` for `named_param`

--- lib/Error/TypeTiny/MissingRequired.pm
+++ lib/Error/TypeTiny/MissingRequired.pm
@@ -3,4 +3,11 @@ package Error::TypeTiny::MissingRequired;
 require Error::TypeTiny;
 our @ISA = 'Error::TypeTiny';
 
+sub named_param{ $_[0]->{named_param} }
+
+sub _build_message{
+    my $e = shift;
+    return "Missing required parameter: " . $e->named_param()
+}
+
 1;
