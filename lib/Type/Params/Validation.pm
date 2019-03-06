5a66019b80d922ce2a47b133bb0ec1b1c060e6c1
Theo van Hoesel <Th.J.v.Hoesel@gmail.com>
2019-03-06 14:39:55 +0100

    add POD - DISCLAIMER OF WARRANTIES

--- lib/Type/Params/Validation.pm
+++ lib/Type/Params/Validation.pm
@@ -138,6 +138,12 @@ This software is copyright (c) 2013-2014, 2017-2019 by Toby Inkster.
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 =cut
 
 1;
